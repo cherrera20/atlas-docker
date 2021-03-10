8d6f2dc9db6512f88ac9edb1904049c9ca465b20
Christian <christian.herrera@schibsted.com>
2021-03-10 17:01:21 +0100

    initial

--- samples/atlas-kafka-cassandra-solr/atlas/scripts/00_create_solr_collections.sh
+++ samples/atlas-kafka-cassandra-solr/atlas/scripts/00_create_solr_collections.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-curl 'http://solr:8983/solr/admin/collections?action=CREATE&name=vertex_index&numShards=1&collection.configName=atlas'
-curl 'http://solr:8983/solr/admin/collections?action=CREATE&name=edge_index&numShards=1&collection.configName=atlas'
-curl 'http://solr:8983/solr/admin/collections?action=CREATE&name=fulltext_index&numShards=1&collection.configName=atlas'
+#curl 'http://solr:8983/solr/admin/collections?action=CREATE&name=vertex_index&numShards=1&collection.configName=atlas'
+#curl 'http://solr:8983/solr/admin/collections?action=CREATE&name=edge_index&numShards=1&collection.configName=atlas'
+#curl 'http://solr:8983/solr/admin/collections?action=CREATE&name=fulltext_index&numShards=1&collection.configName=atlas'
+
+
+curl 'http://localhost:8983/solr/admin/collections?action=CREATE&name=vertex_index&numShards=1&collection.configName=atlas'
+curl 'http://localhost:8983/solr/admin/collections?action=CREATE&name=edge_index&numShards=1&collection.configName=atlas'
+curl 'http://localhost:8983/solr/admin/collections?action=CREATE&name=fulltext_index&numShards=1&collection.configName=atlas'
+
+
+#curl 'http://localhost:8983/solr/admin/collections?action=DELETE&name=vertex_index&numShards=1&collection.configName=atlas'
+#curl 'http://localhost:8983/solr/admin/collections?action=DELETE&name=edge_index&numShards=1&collection.configName=atlas'
+#curl 'http://localhost:8983/solr/admin/collections?action=DELETE&name=fulltext_index&numShards=1&collection.configName=atlas'
+
